eb46b3b73cca7a42fc013719c7a5d7e37857b0eb
Malika7188 <asmanodeny@gmail.com>
2025-06-11 03:59:19 +0300

    added route layout

---
+++ donation-app/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<nav class="p-4 bg-gray-800 text-white">
+	<a href="/" class="mr-4">Home</a>
+</nav>
+
+<main class="p-6">
+	<slot />
+</main>
